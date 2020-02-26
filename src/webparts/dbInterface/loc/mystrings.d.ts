a161548f257914583f2bd5f09cfcd42a43549c9f
Ha Ly <lyha@pdx.edu>
2020-02-25 16:20:05 -0800

    Add initial files

---
+++ src/webparts/dbInterface/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDbInterfaceWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DbInterfaceWebPartStrings' {
+  const strings: IDbInterfaceWebPartStrings;
+  export = strings;
+}
